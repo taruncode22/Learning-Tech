a30edfe58f57fa4e6e4d80541513c557baec8e3b
Tarun gupta <tarunias33@gmail.com>
2025-02-22 14:29:55 +0530

    ADD Comments in the .JS file

--- Web-Development/JavaScript/01_todo-list-with-localstorage/myscript.js
+++ Web-Development/JavaScript/01_todo-list-with-localstorage/myscript.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Element grbbed and stored in variable
   const todoInput = document.getElementById("todo-input");
   const addTaskButton = document.getElementById("add-task-btn");
   const todoList = document.getElementById("todo-list");
+
+  // Pushing tasks into the Array (tasks) and recalling any tasks stored in the local storage
 
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
@@ -22,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
     todoInput.value = ""; //clear input
     console.log(tasks);
   });
-
+  // As soon as the page loads we want to read from the local storage, grab all the tasks, store them inside the array, then run a loop to read all the individual task in the array and then call method renderTask to render every task in the array as a list items
   function renderTask(task) {
     const li = document.createElement("li");
     li.setAttribute("data-id", task.id);
@@ -40,14 +43,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     li.querySelector("button").addEventListener("click", (e) => {
       e.stopPropagation(); //prevent toggle from firing
-      tasks = tasks.filter((t) => t.id === task.id);
-      li.remove();
+      tasks = tasks.filter((t) => t.id === task.id); 
+      li.remove(); //Revomes the task on pressing delete button when id of eventlister is same as the task id 
       saveTasks();
     });
 
     todoList.appendChild(li);
   }
-
+// fuction to add tasks to the local Storage
   function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }
