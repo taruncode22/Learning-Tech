4e50faed0df5d2f653e2947c280351b350a210c4
Tarun gupta <tarunias33@gmail.com>
2025-02-27 11:47:47 +0530

    updated script with minor bugs

--- Web-Development/JavaScript/04_Expense_Tracker/script.js
+++ Web-Development/JavaScript/04_Expense_Tracker/script.js
@@ -1,36 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-const expenseForm = document.getElementById("expense-form")
-const expenseAmountInput = document.getElementById("expense-amount")
-const expenseList = document.getElementById("expense-list")
-const totalAmountDisplay = document.getElementById("total-amount")
-const expenseNameInput = document.getElementById("expense-name")
+    const expenseForm = document.getElementById("expense-form")
+    const expenseAmountInput = document.getElementById("expense-amount")
+    const expenseList = document.getElementById("expense-list")
+    const totalAmountDisplay = document.getElementById("total-amount")
+    const expenseNameInput = document.getElementById("expense-name")
 
-let expense = JSON.parse(localStorage.getItem("expense")
-) || [];
-let totalAmount = calculateTotal();
+    let expense = JSON.parse(localStorage.getItem("expense")
+    ) || [];
+    let totalAmount = calculateTotal();
 
-renderExpenses();
+    renderExpenses();
 
-expenseForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const name = expenseNameInput.ariaValueMax.trim();
+    expenseForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const name = expenseNameInput.ariaValueMax.trim();
 
-    if (name !== "" && !isNaN(amount) && amount > 0 ) {
-        const newExpense = {
-            id:Date.now(),
-            name: name,
-            amount: amount,
-        };
-        expense.push(newExpense)
-        saveExpensesTolocal();
-        renderExpenses();
-        updateTotal();
+        if (name !== "" && !isNaN(amount) && amount > 0) {
+            const newExpense = {
+                id: Date.now(),
+                name: name,
+                amount: amount,
+            };
+            expense.push(newExpense)
+            saveExpensesTolocal();
+            renderExpenses();
+            updateTotal();
 
-        expenseNameInput.value = "";
-        expenseAmountInput.value = "";
-    }
-});
+            expenseNameInput.value = "";
+            expenseAmountInput.value = "";
+        }
+    });
 
     function renderExpenses() {
         expenseList.innerHTML = "";
@@ -42,27 +42,30 @@ expenseForm.addEventListener("submit", (e) => {
             `;
             expenseList.appendChild(li)
         });
-    
+
     }
 
-    
-    
     function calculateTotal() {
-    
+        return expense.reduce((sum, expense) => sum + expense.amount, 0);
     }
-    
+
     function saveExpensesTolocal() {
-    
+        localStorage.setItem("expenses", JSON.stringify(expenses));
     }
-    
+
     function updateTotal() {
-    
+        totalAmount = calculateTotal();
+        totalAmountDisplay.textContent = totalAmount.tofixed(2);
     }
 
+    expenseList.addEventListener("click", (e) => {
+        if (e.target.tagName === "BUTTON") {
+            const expenseId = parseInt(e.target.getAttribute("data-id"));
+            ((expense) => expense.id !== expenseId);
 
-
-
-
-
-
+            saveExpensesTolocal();
+            renderExpenses();
+            updateTotal();
+        }
+    });
 })
